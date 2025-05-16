393cfef604a5796e369b120e285cf5ddb664c0e3
meneerezra <ezraschutte227@gmail.com>
2025-05-16 13:17:52 +0200

    Go init

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("html/*")
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	router.Run(":80")
+}
