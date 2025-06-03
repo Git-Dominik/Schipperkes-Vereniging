1dd07a34f7dbecec9d100a741546c65696476640
meneerezra <ezraschutte227@gmail.com>
2025-06-03 18:07:40 +0200

    Feat: Serve static content and /geschiedenis endpoint

--- main.go
+++ main.go
@@ -8,10 +8,18 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("html/*")
+	router.LoadHTMLGlob("frontend/*.html")
+	router.Static("/styles", "./frontend/styles/")
+	router.Static("/images", "./frontend/images/")
+	router.Static("/scripts", "./frontend/scripts/")
+
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 
-	router.Run(":80")
+	router.GET("/geschiedenis", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "geschiedenis.html", gin.H{})
+	})
+
+	router.Run(":8080")
 }
